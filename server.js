c847694e508162d7ffb477e58497b8bf876de738
norbadrul <norbadrul@live.com>
2020-04-23 10:22:37 +0800

    updates serverjs

--- server.js
+++ server.js
@@ -1,8 +1,6 @@
-
 if (process.env.NODE_ENV !== 'production') {
-    require('dotenv').config({path:'.env'})
-  }
-  
+  require('dotenv').config()
+}  
   const express = require('express')
   const app = express()
   const expressLayouts = require('express-ejs-layouts')
